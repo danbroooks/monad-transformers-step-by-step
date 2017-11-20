864f4443f22a800a34e5e9b93b29758b0d784a9c
Dan Brooks <dmbrooks@live.co.uk>
2017-11-20 22:25:35 +0000

    Complete section 2.1

--- src/Transformers.hs
+++ src/Transformers.hs
@@ -30,17 +30,24 @@ data Value
 
 type Env = Map.Map Name Value
 
-eval :: Env -> Exp -> Value
-eval env (Lit i) = IntVal i
-eval env (Var n) = fromJust (Map.lookup n env)
-eval env (Plus a b) =
-    let IntVal a' = eval env a
-        IntVal b' = eval env b
-    in IntVal (a' + b')
-eval env (Abs n e) = FunVal env n e
-eval env (App a b) =
+type Eval a = Identity a
+
+runEval :: Eval a -> a
+runEval = runIdentity
+
+eval :: Monad m => Env -> Exp -> m Value
+eval env (Lit i) = return $ IntVal i
+eval env (Var n) =
+    case Map.lookup n env of
+        Just n' -> return n'
+        Nothing -> fail ("undefined variable: " ++ n)
+eval env (Plus a b) = do
+    IntVal a' <- eval env a
+    IntVal b' <- eval env b
+    return $ IntVal (a' + b')
+eval env (Abs n e) = return $ FunVal env n e
+eval env (App a b) = do
+    a' <- eval env a
+    b' <- eval env b
     case a' of
         FunVal env' n body -> eval (Map.insert n b' env') body
-  where
-    a' = eval env a
-    b' = eval env b
